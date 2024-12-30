5fa701d226ac0aaaf20b5df21bc4b16c530446f1
Christabel Chukwujama <christabel.jama@gmail.com>
2024-12-30 15:07:42 +0100

    initial commit 🚀

---
+++ turtle-square.py
@@ -0,0 +1,16 @@
+import turtle as t 
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+
+# to get a diagonal 
+import math
+c = 100 * math.sqrt(2)
+t.left(45)
+t.forward(c)
+t.exitonclick()
